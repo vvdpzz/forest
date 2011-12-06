fc81080a0a7f6bb13a58412ca2cff267366b71b9
vvdpzz <vvdpzz@gmail.com>
2011-12-06 14:04:35 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Forest::Application.config.secret_token = '855b5f15785e61f545f2e76c9530832eb2e67ab4164374b4c090b5e86c2e656cb8347aef91a03ee53ffa3bb1786068e362c5a9bcd9df194fce071b496baacda0'
